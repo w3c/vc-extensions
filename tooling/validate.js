47ae62f3070e7dd8bea2a1c8dade4e07b344d347
Manu Sporny <msporny@digitalbazaar.com>
2023-02-18 18:20:04 -0500

    Fix validation tooling URL checking and error output.

--- tooling/validate.js
+++ tooling/validate.js
@@ -6,52 +6,52 @@ const path   = require('path');
 const ajv = new Ajv({ strict: false });
 
 const vcSpecDir = path.join(__dirname, '../specifications');
-
 const schema = yaml.load(fs.readFileSync('./specification-entry.yml', 'utf8'));
 const validate = ajv.compile(schema)
 
 const getAllSpecEntries = () =>{
-    const files = fs.readdirSync(vcSpecDir);
-    const entries = files.filter((file)=>{
-        // ignore the index file.
-        return file !== 'index.json';
-    }).map((file) => {
-        const fileContent =
-          fs.readFileSync(path.join(vcSpecDir, file)).toString();
-        let vcSpec = {
-          error: 'specifications/' + file
-        };
-        try {
-          vcSpec = JSON.parse(fileContent);
-        } catch(e) {
-          console.error('❌ Failed to parse specification entry: ' +
-            'specifications/' + file);
-        }
-        return vcSpec;
-    }).sort((a, b)=>{
-        return a.name > b.name ? 1 : -1;
-    })
-    return entries
+  const files = fs.readdirSync(vcSpecDir);
+  const entries = files.filter((file) => {
+    // ignore the index file.
+    return file !== 'index.json';
+  }).map((file) => {
+    const fileContent =
+      fs.readFileSync(path.join(vcSpecDir, file)).toString();
+    let vcSpec = {
+      error: 'specifications/' + file
+    };
+    try {
+      vcSpec = JSON.parse(fileContent);
+    } catch(e) {
+      console.error('❌ Failed to parse specification entry: ' +
+        'specifications/' + file);
+    }
+    return vcSpec;
+  }).sort((a, b)=>{
+    return a.name > b.name ? 1 : -1;
+  })
+  return entries;
 }
 
-const validateSpecEntry = (entry)=>{
-    const valid = validate(entry)
-    if (!valid) {
-        console.error(entry)
-        console.error(validate.errors)
-    }
-    return valid;
+const validateSpecEntry = (entry) => {
+  const valid = validate(entry)
+  if(!valid) {
+    console.error('Entry:', JSON.stringify(entry, null, 2));
+    console.error('Error:', JSON.stringify(validate.errors, null, 2));
+  }
+  return valid;
 }
 
 (async ()=>{
-    console.log('🧙 validating specification directory');
-    const entries = getAllSpecEntries();
-    entries.forEach((entry)=>{
-        const valid = validateSpecEntry(entry)
-        if (!valid){
-            console.error('❌ invalid specification entry: ' + JSON.stringify(entry, null, 2));
-            process.exit(1);
-        }
-    })
-    console.log('✅ specification directory is valid.');
+  console.log('Validating specification directory...');
+  const entries = getAllSpecEntries();
+  entries.forEach((entry) => {
+    const valid = validateSpecEntry(entry)
+    if(!valid){
+      console.error('   ❌ invalid specification entry: ' +
+        JSON.stringify(entry, null, 2));
+      process.exit(1);
+    }
+  })
+  console.log('   ✅ specification directory is valid.');
 })();
